806a3bc99d7253d209dfcfbdb5100612f37ddb96
Bodya <kkingqz@gmail.com>
2022-08-24 22:05:09 +0300

    +1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-message = 'vVaaabbbbccHello, world!'
+message = 'zZvVaaabbbbccHello, world!'
 offset = 33
 encoded_message = ''
 
 for ch in message:
-    if ord(ch) in range(ord('a'), ord('z')):
+    if ord(ch) in range(ord('a'), ord('z')+1):
         pos = (ord(ch) - ord('a') + offset) % 26
         new_char = chr(pos + ord('a'))
-    elif ord(ch) in range(ord('A'), ord('Z')):
+    elif ord(ch) in range(ord('A'), ord('Z')+1):
         pos = (ord(ch) - ord('A') + offset) % 26
         new_char = chr(pos + ord('A'))
     else:
